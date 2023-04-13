2cec5534790141a691fa2eae6fc68aa8e4cc9d5a
Alan Barry <alanklinect@gmail.com>
2023-04-12 21:04:28 -0500

    fixed error in master, combined query

--- db/init_db.js
+++ db/init_db.js
@@ -8,27 +8,6 @@ async function buildTables() {
   try {
     client.connect();
 
-    await client.query(`
-      create table products (
-          id serial primary key,
-          name varchar(255) unique not null,
-          description varchar(255) not null,
-          price integer not null
-      );
-      create table orders (
-        orderID serial primary key,
-        "orderUserID" references users(id),
-        orderShipName VARCHAR(100),
-        orderShipAddress VARCHAR(100),
-        orderShipAddress2 VARCHAR(100),
-        orderCity VARCHAR(100),
-        orderState VARCHAR(100),
-        orderZip VARCHAR(10),
-        orderEmail VARCHAR(100),
-        orderShipped BOOLEAN default false,
-        orderTrackingNumber VARCHAR(80)
-      );
-    `);
 
     // drop tables in correct order
     await client.query(`
@@ -38,6 +17,25 @@ async function buildTables() {
     DROP TABLES IF EXISTS Categories;
     DROP TABLES IF EXISTS Orders;
 
+    create table products (
+      id serial primary key,
+      name varchar(255) unique not null,
+      description varchar(255) not null,
+      price integer not null
+    );
+    create table orders (
+      orderID serial primary key,
+      "orderUserID" references users(id),
+      orderShipName VARCHAR(100),
+      orderShipAddress VARCHAR(100),
+      orderShipAddress2 VARCHAR(100),
+      orderCity VARCHAR(100),
+      orderState VARCHAR(100),
+      orderZip VARCHAR(10),
+      orderEmail VARCHAR(100),
+      orderShipped BOOLEAN default false,
+      orderTrackingNumber VARCHAR(80)
+    );
     CREATE TABLE Users (
       id SERIAL PRIMARY KEY,
       username varchar(255) UNIQUE NOT NULL,
@@ -47,16 +45,15 @@ async function buildTables() {
       userLastName varchar(255) UNIQUE NOT NULL,
       userLocation varchar(255) NOT NULL,
       active BOOLEAN DEFAULT true
-      
+    );  
     CREATE TABLE active_cart (
       id SERIAL PRIMARY KEY,
       username varchar(255) UNIQUE NOT NULL,
       name  varchar(255) NOT NULL,
       address varchar(255) NOT NULL
-      );
+    );
    `);
 
-    `)
     console.log('finished dropping and creating tables')
     // build tables in correct order
 
