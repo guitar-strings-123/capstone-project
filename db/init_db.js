8718ed2f9430d0df6a17934ad5606147d580b372
seanwangelin <seanwangelin@outlook.com>
2023-04-13 18:45:24 -0500

    fixed typo in users table

--- db/init_db.js
+++ db/init_db.js
@@ -39,7 +39,7 @@ async function buildTables() {
     );
     
 
-    CREATE TABLE Users (
+    CREATE TABLE users (
       id SERIAL PRIMARY KEY,
       username varchar(255) UNIQUE NOT NULL,
       password varchar(255) UNIQUE NOT NULL,
