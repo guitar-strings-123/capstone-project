13b8573cc6bf4a25442d1df2ce7fb58c1312df95
Robert Green <117601081+jamonit16@users.noreply.github.com>
2023-04-12 20:23:24 -0500

    feat: create active_cart table

--- db/init_db.js
+++ db/init_db.js
@@ -2,13 +2,20 @@ const {
   client,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
 
 async function buildTables() {
   try {
     client.connect();
 
     // drop tables in correct order
+    await client.query(`
+        CREATE TABLE active_cart (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          name  varchar(255) NOT NULL,
+          address varchar(255) NOT NULL
+`);
 
     // build tables in correct order
   } catch (error) {
