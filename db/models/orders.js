9c0bf7abbd0b215a8314248856719c8f03c84ab4
Jncjohnson <jarrettncjohnson@yahoo.com>
2023-04-19 20:53:51 -0500

    getAllOrdersByUserID added

--- db/models/orders.js
+++ db/models/orders.js
@@ -1,57 +1,80 @@
-const client = require("../client");
+const client = require('../client');
+module.exports = {};
 
 const getAllOrders = async () => {
-    try {
-        const {rows} = await client.query(`
+  try {
+    const { rows } = await client.query(`
             SELECT *
             FROM orders;
-        `)
+        `);
 
-        return rows
-    } catch (err) {
-        console.log(err) 
-        throw err
-    }
-}
+    return rows;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
 const createOrder = async (
-    orderUserID,
-    orderShipAddress,
-    orderShipAddress2,
-    orderCity,
-    orderState,
-    orderZip,
-    orderEmail,
-    orderShipped,
-    orderTrackingNumber
+  orderUserID,
+  orderShipAddress,
+  orderShipAddress2,
+  orderCity,
+  orderState,
+  orderZip,
+  orderEmail,
+  orderShipped,
+  orderTrackingNumber
 ) => {
-    try {
-        const { rows: [order] } = await client.query(`
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
             INSERT INTO orders("orderUserID", orderShipName, orderShipAddress, orderShipAddress2, orderCity, orderState, orderZip, orderEmail, orderShipped, orderTrackingNumber)
             VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING *;
-        `, [orderUserID,
-            orderShipAddress,
-            orderShipAddress2,
-            orderCity,
-            orderState,
-            orderZip,
-            orderEmail,
-            orderShipped,
-            orderTrackingNumber])
+        `,
+      [
+        orderUserID,
+        orderShipAddress,
+        orderShipAddress2,
+        orderCity,
+        orderState,
+        orderZip,
+        orderEmail,
+        orderShipped,
+        orderTrackingNumber,
+      ]
+    );
 
-        return order
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
+    return order;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+async function getAllOrdersByUserID(userID) {
+  try {
+    const { rows } = await client.query(
+      `
+        SELECT *
+        FROM orders
+        WHERE orderUserID = $1;
+      `,
+      [userID]
+    );
+    if (!rows || !rows.length) return null;
+    const [order] = rows;
+    return order;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
-
-
 
 module.exports = {
-    getAllOrders,
-    createOrder
-}
+  getAllOrders,
+  createOrder,
+  getAllOrdersByUserID,
+};
