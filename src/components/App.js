7a7b7f361025d19671ef9e3f0eeabb936bcd9fbd
bryceAM <bryce.malmberg@yahoo.com>
2023-04-24 21:09:21 -0700

    feat: add homepage route in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Routes, Route} from 'react-router-dom';
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
@@ -28,8 +29,12 @@ const App = () => {
   // render all components below
   return (
     <div className="app-container">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
       <h1>Hello, World!</h1>
       <p>API Status: {APIHealth}</p>
+
     </div>
   );
 };
