1d6b601ca95bb32c6dea19381e1a3f7824a50746
Jncjohnson <jarrettncjohnson@yahoo.com>
2023-04-26 19:38:06 -0500

    initial

---
+++ src/components/ProductCard.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import React from 'react';
+import { getAllProducts } from '../../db/models/products';
+
+export default function ProductCard() {
+  const [products, setProducts] = useState([]);
+
+  const init = async () => {
+    try {
+      const result = await getAllProducts();
+      if (result) {
+        setProducts(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return <div>{products}</div>;
+}
