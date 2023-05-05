da05bbb23d1a20fb83b524d302bab3fe4fdd2c15
Jncjohnson <jarrettncjohnson@yahoo.com>
2023-05-05 08:16:20 -0500

    Simple CSS added to all working components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { FeaturedProducts } from './';
-
 export default function HomePage() {
   return (
     <div className="header">
-      <h1>Welcome to Strum on In!</h1>
+      <img id="welcomeImg" src="https://i.imgur.com/AleLEV6.png" />
       <FeaturedProducts />
     </div>
   );
