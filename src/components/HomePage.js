af6bed90d313e553786ec12277412bec90d61013
bryceAM <bryce.malmberg@yahoo.com>
2023-05-07 15:53:25 -0700

    feat: style homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { FeaturedProducts } from './';
+import { default as FeaturedProducts } from './FeaturedProducts';
+import '../style/HomePage.css'
 
 export default function HomePage() {
+
   return (
-    <div className="header">
-      <h1>Welcome to Strum on In!</h1>
-      <FeaturedProducts />
+    <div id="homeContainer">
+      <div className="heroContainer">
+        <div className="hero">
+          <h1>STRUM ON IN</h1>
+          <h3>Let us be your next adventure</h3>
+        </div>
+      </div>
+      <div className="feature">
+        <FeaturedProducts />
+      </div>
     </div>
   );
 }
